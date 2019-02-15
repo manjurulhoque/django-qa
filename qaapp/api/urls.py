ff975e67a5caf37a43fcb874f6208a83ca596ffb
manjurulhoque <rumimanzurulhoque@gmail.com>
2019-02-15 22:08:42 +0600

    Question create

--- qaapp/api/urls.py
+++ qaapp/api/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
+from rest_framework.routers import DefaultRouter
 
+from .views import QuestionViewSet
 from . import views
+
+router = DefaultRouter()
+router.register('question', QuestionViewSet, base_name='questions')
 
 urlpatterns = [
     path('questions/', views.QuestionListApiView.as_view()),
 ]
+
+urlpatterns += router.urls
--- qaapp/api/views.py
+++ qaapp/api/views.py
@@ -1,11 +1,25 @@
+from rest_framework import viewsets, mixins
 from rest_framework.generics import ListAPIView
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 
 from qaapp.api.custom_pagination import QuestionPagination
+from qaapp.api.custom_permission import CustomIsAuthenticated
 from .serializers import QuestionSerializer
 
 
 class QuestionListApiView(ListAPIView):
     serializer_class = QuestionSerializer
     model = serializer_class.Meta.model
-    queryset = model.objects.all()
+    queryset = model.objects.all().order_by('id')
     pagination_class = QuestionPagination
+
+
+# To disable getting all questions by this viewSet, remove ListModelMixin
+class QuestionViewSet(mixins.CreateModelMixin,
+                      mixins.RetrieveModelMixin,
+                      mixins.UpdateModelMixin,
+                      mixins.DestroyModelMixin,
+                      viewsets.GenericViewSet):
+    serializer_class = QuestionSerializer
+    queryset = serializer_class.Meta.model.objects.all()
+    permission_classes = (CustomIsAuthenticated,) # can be IsAuthenticatedOrReadOnly also
