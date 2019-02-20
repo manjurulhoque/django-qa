688e08199690c6e777cedb9989a0d903c6662f28
manjurulhoque <rumimanzurulhoque@gmail.com>
2019-02-20 15:54:12 +0600

    Updated requirements

--- qa/settings.py
+++ qa/settings.py
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
